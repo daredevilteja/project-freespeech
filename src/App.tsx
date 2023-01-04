6e17a9b93e71a2886897f64f851171419579769f
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2023-01-04 10:56:21 +0530

    Added basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`${styles["container"]}`}>
+      <nav className={`${styles["navBar"]}`}>
+        <span>Home</span>
+        <span>Explore</span>
+        <span>Notifications</span>
+        <span>Messages</span>
+        <span>Profile</span>
+        <span>Tweet</span>
+      </nav>
+      <main className={`${styles["mainContainer"]}`}></main>
+      <section className={`${styles["otherContainer"]}`}></section>
     </div>
   );
 }
